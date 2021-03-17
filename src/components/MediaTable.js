ee4f47c0cbbd0fab06e6efe9fa46a6e1cc5f08db
35840 <osku1993@luukku.com>
2021-03-17 08:39:40 +0200

    second push

---
+++ src/components/MediaTable.js
@@ -0,0 +1,20 @@
+import MediaRow from './MediaRow';
+import PropTypes from 'prop-types';
+
+const MediaTable = ({picArray}) => {
+  return (
+    <table>
+      <tbody>
+      {
+        picArray.map((item, index) => (<MediaRow key={index} file={item}/>))
+      }
+      </tbody>
+    </table>
+  );
+};
+
+MediaTable.propTypes = {
+  picArray: PropTypes.array.isRequired,
+};
+
+export default MediaTable;
