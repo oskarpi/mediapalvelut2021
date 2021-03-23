dd1c7cc0ce558a688995205d0a1acc5e48484791
35840 <osku1993@luukku.com>
2021-03-23 13:25:07 +0200

    first commit

---
+++ src/hooks/ApiHooks.js
@@ -0,0 +1,26 @@
+import {useEffect, useState} from 'react';
+import {baseUrl} from '../utils/variables';
+
+const useAllMedia = () =>{
+  const [picArray, setPicArray] = useState([]);
+
+  useEffect(()=> {
+    const loadMedia = async () => {
+      const response = await fetch(baseUrl + 'media');
+      const files = await response.json();
+      console.log(files);
+
+      const media = await Promise.all(files.map(async (item) => {
+        const resp = await fetch(baseUrl + 'media/' + item.file_id);
+        return resp.json();
+      }));
+
+      setPicArray(media);
+    };
+    loadMedia();
+  }, []);
+
+  return picArray;
+};
+
+export {useAllMedia};
