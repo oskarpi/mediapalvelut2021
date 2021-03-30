799891f9cbc2061ae3aab321a2844bff50bfafa4
35840 <osku1993@luukku.com>
2021-03-30 16:04:55 +0300

    'kesken'

--- src/views/Single.js
+++ src/views/Single.js
@@ -1,14 +1,46 @@
 import {uploadUrl} from '../utils/variables';
 import PropTypes from 'prop-types';
+import {
+  Card,
+  CardActionArea, CardContent, CardMedia,
+  makeStyles,
+  Paper,
+  Typography,
+} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
 
 
 const Single = ({location}) => {
+  const classes = useStyles();
   const file = location.state;
 
   return (
     <>
-      <h1>{file.title}</h1>
-      <img src={uploadUrl + file.filename} alt={file.title}/>
+      <Typography
+        component="h1"
+        variant="h2"
+        gutterBottom>
+        {file.title}</Typography>
+      <Paper elevation="3">
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia className={classes.media}
+              image={uploadUrl + file.filename} title={file.title}/>
+          </CardActionArea>
+          <CardContent>
+            <Typography gutterBottom>{file.description}</Typography>
+            <Typography variant="subtitle2">{file.user_id}</Typography>
+          </CardContent>
+        </Card>
+      </Paper>
     </>
   );
 };
