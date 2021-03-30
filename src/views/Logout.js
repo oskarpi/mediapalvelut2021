398137177f06c8f1d509ed7b77f2a44fbab86e9a
35840 <osku1993@luukku.com>
2021-03-30 15:06:57 +0300

    'viesti'

--- src/views/Logout.js
+++ src/views/Logout.js
@@ -1,6 +1,11 @@
+/* eslint-disable no-unused-vars */
+import {useContext} from 'react';
 import PropTypes from 'prop-types';
+import {MediaContext} from '../contexts/MediaContext';
 
 const Logout = ({history}) => {
+  const [user, setUser] = useContext(MediaContext);
+  setUser(null);
   localStorage.clear();
   history.push('/');
   return (
