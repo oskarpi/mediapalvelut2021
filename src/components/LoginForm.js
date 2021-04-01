e04d49b917346d4a34d9c76bd2d01e3f6505f9c7
35840 <osku1993@luukku.com>
2021-04-01 13:11:36 +0300

    'kesken'

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,6 +4,7 @@ import {useLogin} from '../hooks/ApiHooks';
 import PropTypes from 'prop-types';
 import {withRouter} from 'react-router-dom';
 import {MediaContext} from '../contexts/MediaContext';
+import {Button, Grid, TextField, Typography} from '@material-ui/core';
 
 const LoginForm = ({history}) => {
   const [user, setUser] = useContext(MediaContext);
@@ -11,10 +12,10 @@ const LoginForm = ({history}) => {
 
   const doLogin = async () => {
     try {
-      const userData = await postLogin(inputs);
-      console.log('userData', userData);
-      localStorage.setItem('token', userData.token);
-      setUser(userData.user);
+      const userdata = await postLogin(inputs);
+      console.log('userdata', userdata);
+      localStorage.setItem('token', userdata.token);
+      setUser(userdata.user);
       history.push('/home');
     } catch (e) {
       console.log('doLogin', e.message);
@@ -26,18 +27,56 @@ const LoginForm = ({history}) => {
   console.log('LoginForm', inputs, user);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input name="username" onChange={handleInputChange}
-        value={inputs.username}/>
-      <input name="password" type="password" onChange={handleInputChange}
-        value={inputs.password}/>
-      <button>Kirjaudu</button>
-    </form>
+    <Grid container>
+      <Grid item xs={12}>
+        <Typography
+          component="h1"
+          variant="h2"
+          gutterBottom>Login</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <form onSubmit={handleSubmit}>
+          <Grid container>
+            <Grid container item>
+              <TextField
+                fullWidth
+                type="text"
+                name="username"
+                label="Username"
+                onChange={handleInputChange}
+                value={inputs.username}
+              />
+            </Grid>
+            <Grid container item>
+              <TextField
+                fullWidth
+                type="password"
+                name="password"
+                label="Password"
+                onChange={handleInputChange}
+                value={inputs.password}
+              />
+            </Grid>
+
+            <Grid container item>
+              <Button
+                fullWidth
+                color="primary"
+                type="submit"
+                variant="contained">
+                Login
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+    </Grid>
   );
 };
 
 LoginForm.propTypes = {
   history: PropTypes.object,
 };
+
 
 export default withRouter(LoginForm);
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,13 +1,15 @@
 import useSignUpForm from '../hooks/RegisterHooks';
 import {useUsers} from '../hooks/ApiHooks';
+import {Grid, TextField, Typography, Button} from '@material-ui/core';
 
 const RegisterForm = () => {
   const {postRegister, getUserAvailable} = useUsers();
 
   const doRegister = async () => {
     try {
-      console.log('rekisteröitymislomake lähtee');
+      console.log('rekisteröinti lomake lähtee');
       const available = await getUserAvailable(inputs.username);
+      console.log('availabale', available);
       if (available) {
         postRegister(inputs);
       }
@@ -17,21 +19,75 @@ const RegisterForm = () => {
   };
 
   const {inputs, handleInputChange, handleSubmit} = useSignUpForm(doRegister);
-
   // console.log('RegisterForm', inputs);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input name="username" onChange={handleInputChange}
-        value={inputs.username}/>
-      <input name="password" type="password" onChange={handleInputChange}
-        value={inputs.password}/>
-      <input name="email" type="email" onChange={handleInputChange}
-        value={inputs.email}/>
-      <input name="full_name" onChange={handleInputChange}
-        value={inputs.full_name}/>
-      <button>Rekisteröidy</button>
-    </form>
+    <Grid container>
+      <Grid item xs={12}>
+        <Typography
+          component="h1"
+          variant="h2"
+          gutterBottom>Register</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <form onSubmit={handleSubmit}>
+          <Grid container>
+            <Grid container item>
+              <TextField
+                fullWidth
+                type="text"
+                name="username"
+                label="Username"
+                onChange={handleInputChange}
+                value={inputs.username}
+              />
+            </Grid>
+
+            <Grid container item>
+              <TextField
+                fullWidth
+                type="password"
+                name="password"
+                label="Password"
+                onChange={handleInputChange}
+                value={inputs.password}
+              />
+            </Grid>
+
+            <Grid container item>
+              <TextField
+                fullWidth
+                type="email"
+                name="email"
+                label="Email"
+                onChange={handleInputChange}
+                value={inputs.email}
+              />
+            </Grid>
+
+            <Grid container item>
+              <TextField
+                fullWidth
+                type="text"
+                name="full_name"
+                label="Full name"
+                onChange={handleInputChange}
+                value={inputs.full_name}
+              />
+            </Grid>
+
+            <Grid container item>
+              <Button fullWidth
+                color="primary"
+                type="submit"
+                variant="contained">
+                Register
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+    </Grid>
   );
 };
 
